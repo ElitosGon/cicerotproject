3f20724d3424dc5575fd9534372fa3ac5320f955
ElitosGon <elias.gonzalezma@gmail.com>
2017-05-15 03:17:09 -0400

    Primer commit

---
+++ cicerotproject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cicerotproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cicerotproject.settings")
+
+application = get_wsgi_application()
